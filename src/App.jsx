f769f9cd322bc41050ed0e8edcf6392382848307
Genta <gentapossenti@gmail.com>
2025-06-24 21:12:50 +0700

    updated structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/NavBar";
 import Hero from './components/Hero'
 import About from './components/About'
 import Academy from './components/Academy';
+import Contact from './components/Contact';
+import CTA from './components/CTA';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Hero />
       <About />
       <Academy />
+      <CTA />
+      <Contact />
     </div>
   )
 }
